1ce594c944caaaaf77faa1823b34a276385d0c5b
Victor Alencar <vba.cin@gmail.com>
2019-09-23 14:41:12 -0300

    Exclusão de import desnecessário.

--- src/main/java/log2prov/Main.java
+++ src/main/java/log2prov/Main.java
@@ -2,14 +2,11 @@ package log2prov;
 
 import static language.expressions.ExpressionInterface.TRUE;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
